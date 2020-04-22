e0a9b3bf89805ecb621472af356c3a87c264f0da
Erik Lindh <serik.lindh@gmail.com>
2020-04-22 15:23:19 +0200

    presentkort fixes

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ AOS.init({
     easing: 'slide'
 });
 
-$(function(){ 
+$(function() {
     var navMain = $("#ftco-nav");
-    navMain.on("click", "a", null, function () {
+    navMain.on("click", "a", null, function() {
         navMain.collapse('hide');
     });
 });
@@ -54,3 +54,77 @@ $(document).ready(function($) {
     };
     scrollWindow();
 });
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function validatePresentkortBestallning(mottagareNamn, mottagarePostadress, bestallareEmail, bestallareTel, betalsatt) {
+    var errors = [];
+    if (!mottagareNamn) {
+        errors.push("Ange mottagare namn");
+    }
+
+    if (!mottagarePostadress) {
+        errors.push("Ange mottagare postadress");
+    }
+
+    if (validateEmail(mottagarePostadress)) {
+        errors.push("Ange mottagare postadress INTE e-postadress");
+    }
+
+    if (!bestallareEmail || validateEmail(bestallareEmail) === false) {
+        errors.push("Ange en giltig email för beställare");
+    }
+
+    if (!bestallareTel) {
+        errors.push("Ange beställare telefonnummer");
+    }
+
+    var isValid = errors.length === 0;
+
+    if (isValid === false) {
+        var errorMessage = errors.join("\n");
+        alert(errorMessage);
+    }
+
+    return isValid;
+}
+
+function bestallPresentkort(event) {
+
+    var mottagareNamn = document.getElementById("mottagare-namn-input").value;
+    var mottagarePostadress = document.getElementById("mottagare-postadress-input").value;
+    var bestallareEmail = document.getElementById("bestallare-email-input").value;
+    var bestallareTel = document.getElementById("bestallare-tel-input").value;
+    var betalsatt = document.getElementById("betalsatt-input").value;
+
+    var isValid = validatePresentkortBestallning(mottagareNamn, mottagarePostadress, bestallareEmail, bestallareTel, betalsatt);
+
+    if (isValid) {
+
+        var message = "<h1>Ny beställning av presentkort</h1>" +
+            "Mottagare namn: " + mottagareNamn + "<br/>" +
+            "Mottagare postadress: " + mottagarePostadress + "<br/>" +
+            "Beställare email: " + bestallareEmail + "<br/>" +
+            "Beställare tel: " + bestallareTel + "<br/>" +
+            "Betalsätt: " + betalsatt + "<br/>";
+
+        //sendemail
+
+        var presentkortForm = document.getElementById("presentkort-form");
+        presentkortForm.style.display = 'none';
+
+        var presentkortTackElement = document.getElementById("presentkort-tack-element");
+        presentkortTackElement.style.display = 'block';
+
+        var tackPosition = presentkortTackElement.getBoundingClientRect().top + document.documentElement.scrollTop - 100;
+        window.scrollTo(0, tackPosition);
+    }
+
+
+    event.preventDefault();
+    return false;
+
+}
